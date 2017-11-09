d9fc30c129088f84202ca5e6de39d0bc879f8fb1
teapot2 <33473370+teapot2@users.noreply.github.com>
2017-11-09 15:25:12 -0600

    Add files via upload

---
+++ webcam_test.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+camara = cv2.VideoCapture(0) # Cero es la webcam local
+
+while(True): # Si no esta dentro de un loop solo tomara una foto
+    
+    _, cuadro = camara.read() # Confirmar que si existe un input
+    
+    gray = cv2.cvtColor(cuadro, cv2.COLOR_BGR2GRAY) # Blanco y negro
+    
+    cv2.imshow('gris', gray) # Mostrar la imagen
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+camara.release() # Si no pones esto la camara seguira usandose
