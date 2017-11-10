e0aae8c4b7ef0d4338b5a2c35e0d920cbea3cb66
teapot2 <33473370+teapot2@users.noreply.github.com>
2017-11-10 12:42:53 -0600

    Add files via upload

--- haar_gatherer.py
+++ haar_gatherer.py
@@ -4,13 +4,13 @@ import numpy as np
 import os
 
 def store_raw_images():
-    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
+    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152'
     neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
     
     if not os.path.exists('C:\\Users\\bigse\\Limpiador\\neg'):
         os.makedirs('C:\\Users\\bigse\\Limpiador\\neg')
         
-    pic_num = 1
+    pic_num = 890
     for i in neg_image_urls.split('\n'):
         try:
             print(i)
@@ -23,4 +23,33 @@ def store_raw_images():
         except Exception as e:
             print(str(e))
             
-store_raw_images()
+def find_uglies():
+    match = False
+    for file_type in ['C:\\Users\\bigse\\Limpiador\\neg']:
+        for img in os.listdir(file_type):
+            for ugly in os.listdir('C:\\Users\\bigse\\Limpiador\\Uglies'):
+                try:
+                    current_image_path = str(file_type)+'/'+str(img)
+                    ugly = cv2.imread('C:\\Users\\bigse\\Limpiador\\Uglies\\'+str(ugly))
+                    question = cv2.imread(current_image_path)
+                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+                        print('That is one ugly pic! Deleting!')
+                        print(current_image_path)
+                        os.remove(current_image_path)
+                except Exception as e:
+                    print(str(e))
+                    
+def create_neg():
+    for file_type in ['C:\\Users\\bigse\\Limpiador\\neg']:
+        
+        for img in os.listdir(file_type):            
+                    
+            if file_type == 'C:\\Users\\bigse\\Limpiador\\neg':
+                line = file_type+'\\'+img+'\n'
+                with open('bg.txt','a') as f:
+                    f.write(line)
+                    
+create_neg()
+                    
+#find_uglies()           
+#store_raw_images()
--- il_funcs.py
+++ il_funcs.py
@@ -4,10 +4,12 @@ def redimensionar_foto( path, output, factor ):
     var = cv2.imread(path)
     var = cv2.cvtColor(var, cv2.COLOR_BGR2GRAY)
     height, width = var.shape
-    res = cv2.resize(var, None, fx=(1/factor), fy=(1/factor), interpolation = cv2.INTER_CUBIC)
+    res = cv2.resize(var, (factor, factor), interpolation = cv2.INTER_CUBIC)
     cv2.imwrite(output, res)
     
 def resizeVideo( inp, factor ):
     var = inp
     height, width = var.shape
     inp = cv2.resize(var, None, fx=(1/factor), fy=(1/factor), interpolation = cv2.INTER_CUBIC)
+    
+redimensionar_foto("C:\\Users\\bigse\\Limpiador\\Fotos\\Positives\\9_9.jpg", 'C:\\Users\\bigse\\Limpiador\\Fotos\\Positives\\sample1.jpg', 50)
